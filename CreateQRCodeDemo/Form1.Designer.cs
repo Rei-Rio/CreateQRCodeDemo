1f3ddeaea16937fb3e0b910c33fb84f259906d2d
Rei-Rio <erimei9x@gmail.com>
2021-04-14 10:59:06 +0700

    Add project files.

---
+++ CreateQRCodeDemo/Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿
+namespace CreateQRCodeDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.txtQRcode = new System.Windows.Forms.TextBox();
+            this.cboDevice = new System.Windows.Forms.ComboBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtLink = new System.Windows.Forms.TextBox();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.picQRcode = new System.Windows.Forms.PictureBox();
+            this.txtWidth = new System.Windows.Forms.TextBox();
+            this.txtHeight = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.labelMessage = new System.Windows.Forms.Label();
+            this.txtImport = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picQRcode)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.pictureBox1.Location = new System.Drawing.Point(13, 59);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(613, 435);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(14, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Camera";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(827, 77);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 28);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // txtQRcode
+            // 
+            this.txtQRcode.Location = new System.Drawing.Point(643, 59);
+            this.txtQRcode.Multiline = true;
+            this.txtQRcode.Name = "txtQRcode";
+            this.txtQRcode.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtQRcode.Size = new System.Drawing.Size(165, 57);
+            this.txtQRcode.TabIndex = 3;
+            // 
+            // cboDevice
+            // 
+            this.cboDevice.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboDevice.FormattingEnabled = true;
+            this.cboDevice.Location = new System.Drawing.Point(87, 21);
+            this.cboDevice.Name = "cboDevice";
+            this.cboDevice.Size = new System.Drawing.Size(173, 24);
+            this.cboDevice.TabIndex = 4;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(632, 161);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Link:";
+            // 
+            // txtLink
+            // 
+            this.txtLink.Location = new System.Drawing.Point(686, 161);
+            this.txtLink.Name = "txtLink";
+            this.txtLink.Size = new System.Drawing.Size(134, 22);
+            this.txtLink.TabIndex = 6;
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(707, 190);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(85, 32);
+            this.btnGenerate.TabIndex = 7;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // picQRcode
+            // 
+            this.picQRcode.Location = new System.Drawing.Point(671, 297);
+            this.picQRcode.Name = "picQRcode";
+            this.picQRcode.Size = new System.Drawing.Size(149, 135);
+            this.picQRcode.TabIndex = 8;
+            this.picQRcode.TabStop = false;
+            // 
+            // txtWidth
+            // 
+            this.txtWidth.Location = new System.Drawing.Point(697, 228);
+            this.txtWidth.Name = "txtWidth";
+            this.txtWidth.Size = new System.Drawing.Size(111, 22);
+            this.txtWidth.TabIndex = 9;
+            // 
+            // txtHeight
+            // 
+            this.txtHeight.Location = new System.Drawing.Point(697, 258);
+            this.txtHeight.Name = "txtHeight";
+            this.txtHeight.Size = new System.Drawing.Size(111, 22);
+            this.txtHeight.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(635, 231);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 17);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Width";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(634, 258);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 17);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Height";
+            // 
+            // labelMessage
+            // 
+            this.labelMessage.AutoSize = true;
+            this.labelMessage.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.labelMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelMessage.Location = new System.Drawing.Point(640, 460);
+            this.labelMessage.Name = "labelMessage";
+            this.labelMessage.Size = new System.Drawing.Size(0, 17);
+            this.labelMessage.TabIndex = 12;
+            // 
+            // txtImport
+            // 
+            this.txtImport.Location = new System.Drawing.Point(827, 332);
+            this.txtImport.Name = "txtImport";
+            this.txtImport.Size = new System.Drawing.Size(75, 31);
+            this.txtImport.TabIndex = 13;
+            this.txtImport.Text = "Import";
+            this.txtImport.UseVisualStyleBackColor = true;
+            this.txtImport.Click += new System.EventHandler(this.txtImport_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(910, 506);
+            this.Controls.Add(this.txtImport);
+            this.Controls.Add(this.labelMessage);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtHeight);
+            this.Controls.Add(this.txtWidth);
+            this.Controls.Add(this.picQRcode);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.txtLink);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cboDevice);
+            this.Controls.Add(this.txtQRcode);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "QRCode";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picQRcode)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox txtQRcode;
+        private System.Windows.Forms.ComboBox cboDevice;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtLink;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.PictureBox picQRcode;
+        private System.Windows.Forms.TextBox txtWidth;
+        private System.Windows.Forms.TextBox txtHeight;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label labelMessage;
+        private System.Windows.Forms.Button txtImport;
+    }
+}
+
---
+++ CreateQRCodeDemo/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using ZXing;
+
+namespace CreateQRCodeDemo
+{
+    public partial class Form1 : Form
+    {
+        bool IsCorrect;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        FilterInfoCollection filterInfoCollection;
+        VideoCaptureDevice captureDevice;
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            captureDevice = new VideoCaptureDevice(filterInfoCollection[cboDevice.SelectedIndex].MonikerString);
+            captureDevice.NewFrame += CaptureDevice_NewFrame;
+            captureDevice.Start();
+            timer1.Start();
+        }
+
+        private void CaptureDevice_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            pictureBox1.Image = (Bitmap)eventArgs.Frame.Clone();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            filterInfoCollection = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo filterInfo in filterInfoCollection)
+                cboDevice.Items.Add(filterInfo.Name);
+            cboDevice.SelectedIndex = 0;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (captureDevice.IsRunning)
+                captureDevice.Stop();    
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if(pictureBox1.Image != null)
+            {
+                BarcodeReader barcodeReader = new BarcodeReader();
+                Result result = barcodeReader.Decode((Bitmap)pictureBox1.Image);
+                if (result != null)
+                {
+                    txtQRcode.Text = result.ToString();
+                    timer1.Stop();
+                    Process.Start(txtQRcode.Text);
+                    if (captureDevice.IsRunning)
+                        captureDevice.Stop();
+                }    
+            }    
+        }
+        
+        
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            var url = string.Format("http://chart.apis.google.com/chart?cht=qr&chs={1}x{2}&chl={0}", txtLink.Text, txtWidth.Text, txtHeight.Text);
+            WebResponse response = default(WebResponse);
+            Stream remoteStream = default(Stream);
+            StreamReader readStream = default(StreamReader);
+            WebRequest request = WebRequest.Create(url);
+            response = request.GetResponse();
+            remoteStream = response.GetResponseStream();
+            readStream = new StreamReader(remoteStream);
+            Image img = Image.FromStream(remoteStream);
+            if (IsUrlValid(txtLink.Text))
+            {
+                IsCorrect = true;
+            }
+            else
+            {
+                MessageBox.Show("Format Error!!", "Failed");
+                IsCorrect = false;
+                return;
+            }
+            img.Save(@"D:\" + txtLink.Text + ".png");
+            picQRcode.Image = img;
+            picQRcode.SizeMode = PictureBoxSizeMode.StretchImage;
+            response.Close();
+            remoteStream.Close();
+            readStream.Close();
+            txtLink.Text = string.Empty;
+            txtWidth.Text = string.Empty;
+            txtHeight.Text = string.Empty;
+            labelMessage.Text = "The QR Code is generated!";
+        }
+
+        private void txtImport_Click(object sender, EventArgs e)
+        {
+            if (!IsCorrect)
+            {
+                return;
+            }
+            BarcodeReader barcodeReader = new BarcodeReader();
+            Result result = barcodeReader.Decode((Bitmap)picQRcode.Image);
+            txtQRcode.Text = result.ToString();
+            Process.Start(xuliLink(txtQRcode.Text));
+       
+        }
+        private String xuliLink(String s)
+        {
+            String s1 = "http://www";
+            if (s.IndexOf(s1) < 0)
+            {
+                s = s1 + s;
+            }
+            return s;
+        }
+        private bool IsUrlValid(string url)
+        {
+            string pattern = @"^(http|https|ftp|)\://|[a-zA-Z0-9\-\.]+\.[a-zA-Z](:[a-zA-Z0-9]*)?/?([a-zA-Z0-9\-\._\?\,\'/\\\+&amp;%\$#\=~])*[^\.\,\)\(\s]$";
+            Regex reg = new Regex(pattern, RegexOptions.Compiled | RegexOptions.IgnoreCase);
+            return reg.IsMatch(url);
+        }
+    }
+}
